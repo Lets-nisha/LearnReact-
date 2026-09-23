62bf22de92f47d45d8efee4fa3cd109383660c8a
aqsa-hasan <aqsa.turki786@gmail.com>
2026-09-23 22:18:18 +0530

    add ToDoLocalStgContextsApi project

---
+++ ToDoLocalStgContextsApi/src/contexts/ToDoContexts.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+const ToDoContexts = createContext({
+    todo: [
+        {
+            id: 1,
+            todo: "Todo Mess",
+            completed: false
+        },
+    ],
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { }
+})
+
+export const useToDo = () => {
+    return useContext(ToDoContexts);
+}
+
+
+export const ToDoProvider = ToDoContexts.Provider
